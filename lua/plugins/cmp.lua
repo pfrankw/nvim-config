c458f37cbca9a1f7c63dd97e83783c3f19ac5118
Francesco Pompo <pfrankw@users.noreply.github.com>
2025-04-22 00:07:42 +0200

    Removed codecompanion

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -33,11 +33,8 @@ return {
                 },
                 {
                     { name = 'nvim_lsp_signature_help' }
-                },
-                {
-                    { name = 'codecompanion' }
-                },
-           ),
+                }
+          ),
         })
     end
 }
